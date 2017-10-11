d61cc0f76c2f1101849a0be33273a301ad6b06dc
daidorian09 <yigitat1992@gmail.com>
2017-10-11 18:26:56 +0300

    Add image manipulation service, refactor code

--- RestPostTestApp/Controllers/ImageUploadApiController.cs
+++ RestPostTestApp/Controllers/ImageUploadApiController.cs
@@ -4,82 +4,112 @@ using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using System.Threading.Tasks;
 using SixLabors.ImageSharp;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Net;
 
 namespace RestPostTestApp.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/upload/")]
     public class ImageUploadApiController : Controller
     {
         #region Member
 
         private readonly IHostingEnvironment _hostingEnvironment;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
         #endregion
 
         #region Ctor
-        public ImageUploadApiController(IHostingEnvironment hostingEnvironment)
+        public ImageUploadApiController(IHostingEnvironment hostingEnvironment, IHttpContextAccessor httpContextAccessor)
         {
             _hostingEnvironment = hostingEnvironment;
+            _httpContextAccessor = httpContextAccessor;
         }
         #endregion
 
-        //// GET api/values
-        //[HttpGet]
-        //public IEnumerable<string> Get()
-        //{
-        //    return new string[] { "value1", "value2" };
-        //}
 
-
-        [HttpGet]
-        public string GetManipulation()
+        #region Get
+        //Get api/imageuploaapi/upload/file.png
+        [HttpGet("{filename}")]
+        public IActionResult GetImage(string filename)
         {
-            var isImageUploaded = _hostingEnvironment.WebRootFileProvider.GetFileInfo("/uploads/test.png").Exists;    
+            int queryCount =(int) _httpContextAccessor.HttpContext?.Request?.Query.Count;
+            int widthCount = (int)_httpContextAccessor.HttpContext?.Request?.Query["w"].Count;
+            int heightCount = (int)_httpContextAccessor.HttpContext?.Request?.Query["h"].Count;
+
+            if (queryCount <= 0 ||  widthCount == 0 && heightCount == 0)
+                return NotFound("File is not found");
             
+            int width = 0;
+            int height = 0;
+
+            bool isImageUploaded = _hostingEnvironment.WebRootFileProvider.GetFileInfo("/uploads/" + filename).Exists;
+
             if (!isImageUploaded)
-                return "file not found";
+                return StatusCode((int)HttpStatusCode.Conflict);
+
             else
             {
-                var filePath = _hostingEnvironment.WebRootFileProvider.GetFileInfo("/uploads/test.png").PhysicalPath;
+                string filePath = _hostingEnvironment.WebRootFileProvider.GetFileInfo("/uploads/" + filename).PhysicalPath;
+
                 using (Image<Rgba32> image = Image.Load(filePath))
                 {
-                    image.Mutate(x => x
-                         .Resize(image.Width / 2, image.Height / 2)
-                         .Grayscale());
-                    image.Save("bar.jpg"); // automatic encoder selected based on extension.
+                    width = widthCount == 0 ? image.Width * height / image.Height : Convert.ToInt32(_httpContextAccessor.HttpContext?.Request?.Query["w"]);
+                    height = heightCount == 0 ? image.Height * width / image.Width : Convert.ToInt32(_httpContextAccessor.HttpContext?.Request?.Query["h"]);
+                
+                    bool isDirectoryExistent = Directory.Exists(_hostingEnvironment.WebRootPath + "/uploads/" + width + "X" + height);
+
+                    if (!isDirectoryExistent)
+                        Directory.CreateDirectory(_hostingEnvironment.WebRootPath + "/uploads/" + width + "X" + height);
+
+                    bool isDirectoryFileExistent = _hostingEnvironment.WebRootFileProvider.GetFileInfo("/uploads/" + width + "X" + height + "/" + filename).Exists;
+
+                    if (isDirectoryFileExistent)
+                        return File(System.IO.File.OpenRead(_hostingEnvironment.WebRootFileProvider.GetFileInfo("/uploads/" + width + "X" + height + "/" + filename).PhysicalPath), "image/jpeg");
+
+                    image.
+                        Mutate(x => x
+                              .Resize(width, height)
+                        );
+                    image.Save(_hostingEnvironment.WebRootPath + $"/uploads/" + width + "X" + height + "/" + filename);
                 }
             }
-                return "ok";
+            return File(System.IO.File.OpenRead(_hostingEnvironment.WebRootFileProvider.GetFileInfo("/uploads/" + width + "X" + height + "/" + filename).PhysicalPath), "image/jpeg");
         }
 
-        //// GET api/values/5
-        //[HttpGet("{id}")]
-        //public string Get(int id)
-        //{
-        //    return "value";
-        //}
+        #endregion
 
-        // POST api/values
+
+
+        #region Post
+
+        // POST api/imageuploaapi/upload
         [HttpPost(Name = "Create")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Post(ImageUploadViewModel model)
+        public async Task<IActionResult> UploadFile(ImageUploadViewModel model)
         {
             if(model.File is null)
                 return BadRequest(error: $"File cannot be empty");
             else
             {
-                var isDirectoryExistent = Directory.Exists(_hostingEnvironment.WebRootPath + "/uploads");
+                bool isDirectoryExistent = Directory.Exists(_hostingEnvironment.WebRootPath + "/uploads");
 
                 if (!isDirectoryExistent)
                     Directory.CreateDirectory(_hostingEnvironment.WebRootPath + "/uploads");
 
-                var upload = Path.Combine(_hostingEnvironment.WebRootPath, "uploads");
-                using (var fileStream = new FileStream(Path.Combine(upload, model.File.FileName), FileMode.Create))
+                string uploadPath = Path.Combine(_hostingEnvironment.WebRootPath, "uploads");
+                string fileExtension = Path.GetExtension(model.File.FileName);
+                string fileName = Guid.NewGuid().ToString("N") + fileExtension;
+
+                using (var fileStream = new FileStream(Path.Combine(uploadPath, fileName), FileMode.Create))
                 {
                     await model.File.CopyToAsync(fileStream);
                     return CreatedAtRoute("Create", value: "File is successfully uploaded");
                 }
             }
         }
+
+        #endregion
     }
 }
